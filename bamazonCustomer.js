ce5e7fd1a3a09023f1f707991ff9b42c9f51e0cd
Lexi <you@example.com>
2017-07-21 13:39:38 -0500

    tweaks

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,12 +5,9 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "meemee2125",
     database: "bamazonDB"
 });
-
-var stock = "";
-
 
 connection.connect(function(err) {
     if (err) throw err;
@@ -18,39 +15,36 @@ connection.connect(function(err) {
     printStock();
 });
 
+
 function printStock() {
     connection.query("SELECT * FROM products", function(err, res) {
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
         }
-        console.log("-----------------------------------");
-        shopNow();
+        console.log("-----------------------------------\n");
+        runBamazon();
     });
-};
+}
 
-
-function shopNow() {
-	inquirer.prompt([
-	{
-		name: "select",
-		type: "input",
-		message: "What is the id of the product you'd like to buy?",
-		validate: function(input) {
-			if (input === "") {
-				console.log("Please provide the id of the product you wish to purchase.");
-				return false;
-			} else {
-				return true;
-			}
-		}
-	}
-	]).then(function(answer) {
-		inquirer.prompt([
-		{
-			name: "amount",
-			type: "input",
-			message: "How many units of this product would you like to purchase?",
-			 validate: function(input) {
+function runBamazon() {
+    inquirer.prompt([{
+            type: "input",
+            name: "item_id",
+            message: "Please provide the id of the product you wish to purchase.",
+            validate: function(input) {
+                if (input === "") {
+                    console.log("You have not entered a valid id number.Please provide the id number of the item you are interested in.");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many units of this product would you like to purchase?",
+            validate: function(input) {
                 if (input === "") {
                     console.log("Please specify how many units you'd like.");
                     return false;
@@ -58,19 +52,25 @@ function shopNow() {
                     return true;
                 }
             }
-		}
-		])
-		// checkStock();
-	});
-};
 
-// function checkStock() {
-// 	connection.query (
-// 		"SELECT FROM products WHERE item_id =?", ["input"]
-// 		)
-// 	if (stock.input >= 0) {
-// 		console.log("Thank you for your purchase!");
-// 	} else {
-// 		console.log("Sorry, we do not have")
-// 	}
-// }
+        }
+    ]).then(function(input) {
+        connection.query("SELECT * FROM products WHERE ?", { item_id: input.item_id }, function(err, res) {
+            if (err) throw err;
+            if (input.quantity < res[0].stock_quantity) {
+                // Update the inventory
+                connection.query("UPDATE products SET stock_quantity = " + (res[0].stock_quantity - input.quantity) + " WHERE item_id = " + input.item_id, function(err, data) {
+                    if (err) throw err;
+                    console.log("Thank you for your order! Your total is $" + res[0].price * input.quantity);
+                    console.log("\n---------------------------------------------------------------------\n");
+                    printStock();
+                    // End the database connection
+                    connection.end();
+                })
+            } else {
+                console.log("Sorry, we do not have enough items to fulfill your request.");
+                console.log("\n---------------------------------------------------------------------\n");
+            }
+        })
+    })
+}
